85517bae6a4021080348ab9feea234fdf8af65c4
wangqiuli0102 <418674878@qq.com>
2018-06-27 22:02:29 +0800

    add files

---
+++ csvTools.py
@@ -0,0 +1,41 @@
+'''
+provided by LUNA16
+evaluation script
+'''
+
+import csv
+
+def writeCSV(filename, lines):
+    with open(filename, "w", newline='') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerows(lines)
+
+def readCSV(filename):
+    lines = []
+    with open(filename, "r") as f:
+        csvreader = csv.reader(f)
+        for line in csvreader:
+            lines.append(line)
+    return lines
+
+def tryFloat(value):
+    try:
+        value = float(value)
+    except:
+        value = value
+    
+    return value
+
+def getColumn(lines, columnid, elementType=''):
+    column = []
+    for line in lines:
+        try:
+            value = line[columnid]
+        except:
+            continue
+            
+        if elementType == 'float':
+            value = tryFloat(value)
+
+        column.append(value)
+    return column
---
+++ getNodulemalignancy.py
@@ -0,0 +1,47 @@
+'''
+Created by Wang Qiu Li
+6/27/2018
+'''
+
+import csvTools
+
+metadatas = csvTools.readCSV('csv_xls/LIDC-IDRI_MetaData.csv')
+print(len(metadatas))
+# print(metadatas[0])
+
+'''
+10 column
+['Collection', 'Patient Id', 'Study Date', 
+ 'Study Description', 'Modality', 'Series Description',
+ 'Manufacturer', 'Manufacturer Model', 'Software Version', 'Series UID']
+'''
+
+'''
+LIDC-IDRI-0132
+LIDC-IDRI-0151
+LIDC-IDRI-0315
+LIDC-IDRI-0332
+LIDC-IDRI-0355
+LIDC-IDRI-0365
+LIDC-IDRI-0442
+LIDC-IDRI-0484
+has two CT scans
+
+1012 + 6 = 1018
+'''
+CTList = []
+temp = []
+sign = 0
+for metadata in metadatas:
+    if sign != 0:
+        # print(len(metadata))
+        # print(metadata[4])
+        if metadata[4] == 'CT':
+            ctdata = []
+            ctdata.append(metadata[1])
+            ctdata.append(metadata[9])
+            CTList.append(ctdata)
+    sign += 1
+print(len(CTList))
+
+csvTools.writeCSV('PatientId_SeriesUID.csv', CTList)
