49e4dd6afa5ec7ad4f2c8c99a2ea6ac6972ace93
wangqiuli0102 <418674878@qq.com>
2018-12-13 21:39:09 +0800

    update

--- get_item.py
+++ get_item.py
@@ -1,21 +1,34 @@
+#-*- coding: UTF-8 -*-
+
 import pydicom
 import cv2
+import os
+
+dir = 'testdicom/'
+
+# filelist = os.listdir(dir)
+# selist = []
+# for onefile in filelist:
+#     ds = pydicom.dcmread(dir + onefile)
+#     direct = ds.dir()
+#     if 'SeriesNumber' in direct:
+#         if ds.data_element('SeriesNumber').value == 5:
+#             print(ds.data_element('SliceThickness').value)
+#         # if ds.data_element('SliceThickness').value not in selist:
+#         #     selist.append(ds.data_element('SliceThickness').value)
+
+# print(selist)
 
 ds = pydicom.dcmread('FILE1.dcm')
-print(type(ds))
-item1 = ds.data_element('SliceThickness')
-item2 = ds.data_element('PixelSpacing')
+print(type(ds.dir()))
+for elem in ds.dir():
+    if elem == 'PatientSex':
+        contant = ds.data_element(elem)
+        print(contant.value)
+print('\n\n')
 
-print(item1.value)
-print(item2.value)
 
-ds = pydicom.dcmread('000001.dcm')
-print(type(ds))
-item1 = ds.data_element('SliceThickness')
-item2 = ds.data_element('PixelSpacing')
 
-print(item1.value)
-print(item2.value)
 
 # onepic = ds.pixel_array
 # clahe = cv2.createCLAHE(clipLimit = 2.0, tileGridSize = (8,8))
